fb9ca81451e09541f452655c138c721c35d2be56
nikita evdokimov <evdokimovnikita1@gmail.com>
2022-04-29 03:01:38 +0400

    working with Images and urls

--- RickAndMorthy/Network/Network.swift
+++ RickAndMorthy/Network/Network.swift
@@ -11,29 +11,6 @@ final class  Network {
     
     static let shared = Network()
     
-//    var fetchedData: PostModel?
-    
-//    fileprivate var url: String = "https://rickandmortyapi.com/api/character"
-    
-//    func fetchCharactersList(onCompletion: @escaping ([Result]?) -> ()) {
-//        guard let url = URL(string: url) else {
-//            return
-//        }
-//        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-//            guard let data = data else {
-//                print("data was nil")
-//                return
-//            }
-//            guard let ListOfCharacters = try? JSONDecoder().decode(PostModel.self, from: data) else {
-//                    print("couldn't decode JSON")
-//                return
-//            }
-//            onCompletion(ListOfCharacters.results)
-//        }
-//        task.resume()
-//    }
-    
-    
     func fetchPostModel(url: String, onCompletion: @escaping (PostModel?) -> ()) {
         guard let url = URL(string: url) else {
             return
@@ -51,35 +28,4 @@ final class  Network {
         }
         task.resume()
     }
-    
-    
-    func changeUrl() {
-//        guard let data = fetchedData else { return }
-//        guard let info = data.info else { return }
-//        guard let next = info.next else { return }
-//        self.url = next
-//        print("URL - \(url)")
-    }
 }
-
-//extension ViewController {
-//    func fetchDataFromAPI() {
-//        let anonymousFunction = { (fetchedCharacterList: [Result]?) in
-//            DispatchQueue.main.async {
-//                self.listOfCharacters = fetchedCharacterList!
-//
-////                print(fetchedCharacterList)
-//
-//                self.tableView.reloadData()
-//            }
-//        }
-//        Network.shared.fetchCharactersList(onCompletion: anonymousFunction)
-//        self.tableView.reloadData()
-//    }
-//}
-
-
-
-
-
-
--- RickAndMorthy/View/CharacterCell.swift
+++ RickAndMorthy/View/CharacterCell.swift
@@ -13,7 +13,7 @@ final class CharacterCell: UITableViewCell {
     let nameLabel = UILabel()
     let species = UILabel()
     let gender = UILabel()
-    let avatarImageView = UIImageView()
+    let avatarImageView = UIImageView(image: UIImage(named: "loading"))
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -33,8 +33,9 @@ final class CharacterCell: UITableViewCell {
         contentView.addSubview(avatarImageView)
         
         self.setNameLabel()
+//        avatarImageView.frame(forAlignmentRect: CGRect(origin: .init(x: 0, y: 0), size: CGSize(width: 50, height: 50)))
         self.setAvatarImageView()
-        
+//        avatarImageView.makeRounded()
     }
     
     required init?(coder: NSCoder) {
@@ -61,12 +62,34 @@ extension CharacterCell {
         avatarImageView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
-            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            avatarImageView.bottomAnchor.constraint(equalTo: nameLabel.topAnchor),
-            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+//            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+//            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+//            avatarImageView.bottomAnchor.constraint(equalTo: nameLabel.topAnchor),
+//            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            
 //            avatarImageView.heightAnchor.constraint(equalToConstant: 50)
+            
+            avatarImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            avatarImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            
+            
+            avatarImageView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 1.0 / 2.0),
+            avatarImageView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 1.0 / 2.0)
+//
         ])
+    }
+}
+
+extension UIImageView {
+
+    func makeRounded() {
+
+        self.layer.borderWidth = 1
+        self.layer.masksToBounds = false
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.cornerRadius = self.frame.height / 2
+        self.clipsToBounds = true
     }
 }
 
@@ -75,15 +98,17 @@ extension CharacterCell {
         self.nameLabel.text = character.name
         self.gender.text = character.gender
         
-        if let url = URL(string: character.image!) {
-            let task = URLSession.shared.dataTask(with: url) { data, response, error in
-                guard let data = data, error == nil else { return }
-                
-                DispatchQueue.main.async {
-                    self.avatarImageView.image = UIImage(data: data)
-                }
-            }
-            task.resume()
-        }
+        //MARK: - image from URL
+//        if let url = URL(string: character.image!) {
+//            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+//                guard let data = data, error == nil else { return }
+//
+//                DispatchQueue.main.async {
+//                    self.avatarImageView.image = UIImage(data: data)
+//                }
+//            }
+//            task.resume()
+//        }
     }
 }
+
