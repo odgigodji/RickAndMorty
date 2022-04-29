b38955460f780fa70733d0a1b9053883e0bfb779
nikita evdokimov <evdokimovnikita1@gmail.com>
2022-04-30 00:55:57 +0400

    scroll view work without UIviews

--- RickAndMorthy/View/DetailVC.swift
+++ RickAndMorthy/View/DetailVC.swift
@@ -87,6 +87,37 @@ final class DetailVC: UIViewController {
         return view
     }()
     
+    private let subView1: UIView = {
+        let view = UIView()
+        view.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        view.backgroundColor = UIColor.blue
+        return view
+    }()
+    
+    private let subview2: UIView = {
+        let view = UIView()
+        view.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        view.backgroundColor = UIColor.cyan
+    return view
+    }()
+    
+    private let subview3: UIView = {
+        let view = UIView()
+        view.heightAnchor.constraint(equalToConstant: 400).isActive = true
+        view.backgroundColor = UIColor.gray
+    return view
+    }()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 //        self.modalPresentationStyle = .fullScreen
@@ -94,8 +125,8 @@ final class DetailVC: UIViewController {
         safeArea = view.layoutMarginsGuide
         
         setupScrollView()
-        setupImage()
-        setupData()
+//        setupImage()
+//        setupData()
     }
     
     private func setupImage() {
@@ -121,6 +152,29 @@ final class DetailVC: UIViewController {
 }
 
 //MARK: - setupScrollView
+extension DetailVC {
+    private func setupScrollView() {
+        let margins = view.layoutMarginsGuide
+        view.addSubview(scrollView)
+        scrollView.addSubview(scrollStackViewContainer)
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: margins.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: margins.bottomAnchor).isActive = true
+        scrollStackViewContainer.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        scrollStackViewContainer.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+        scrollStackViewContainer.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        scrollStackViewContainer.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        scrollStackViewContainer.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+        configureContainerView()
+    }
+    
+    private func configureContainerView() {
+        scrollStackViewContainer.addArrangedSubview(subView1)
+        scrollStackViewContainer.addArrangedSubview(subview2)
+        scrollStackViewContainer.addArrangedSubview(subview3)
+    }
+}
 //extension DetailVC {
 //    private func setupScrollView() {
 //        print("setup scroll view")
