d9d25b12034879aa0c3a093311cbfd11271ef284
nikita evdokimov <evdokimovnikita1@gmail.com>
2022-04-30 01:27:38 +0400

    image work on ScrollView

--- RickAndMorthy/View/DetailVC.swift
+++ RickAndMorthy/View/DetailVC.swift
@@ -67,12 +67,12 @@ final class DetailVC: UINavigationController {
 final class DetailVC: UIViewController {
     //MARK: - members of DetailVC
     var character : Result?
-    var safeArea : UILayoutGuide!
+//    var safeArea : UILayoutGuide!
     let avatarImageView = CustomImageView()
 //    let scrollView = UIScrollView()
 //    let contentView = UIView()
     
-    //MARK: - sctollView constants
+    //MARK: - scrollView constants
     private let scrollView: UIScrollView = {
         let view = UIScrollView()
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -87,16 +87,25 @@ final class DetailVC: UIViewController {
         return view
     }()
     
-    private let subView1: UIView = {
-        let view = UIView()
-        view.heightAnchor.constraint(equalToConstant: 200).isActive = true
-        view.backgroundColor = UIColor.blue
-        return view
+    private let subView1 : CustomImageView = {
+        let IV = CustomImageView()
+        IV.translatesAutoresizingMaskIntoConstraints = false
+        IV.widthAnchor.constraint(equalToConstant: 400).isActive = true
+//        IV.widthAnchor.constraint(equalToConstant: 400).isActive = true
+        IV.heightAnchor.constraint(equalTo: IV.widthAnchor,constant: 20).isActive = true
+        return IV
     }()
+    
+//    private let subView1: UIView = {
+//        let view = UIView()
+//        view.heightAnchor.constraint(equalToConstant: 400).isActive = true
+//        view.backgroundColor = UIColor.blue
+//        return view
+//    }()
     
     private let subview2: UIView = {
         let view = UIView()
-        view.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        view.heightAnchor.constraint(equalToConstant: 400).isActive = true
         view.backgroundColor = UIColor.cyan
     return view
     }()
@@ -122,23 +131,23 @@ final class DetailVC: UIViewController {
         super.viewDidLoad()
 //        self.modalPresentationStyle = .fullScreen
         view.backgroundColor = .systemGray2
-        safeArea = view.layoutMarginsGuide
+//        safeArea = view.layoutMarginsGuide
         
         setupScrollView()
-//        setupImage()
-//        setupData()
+        setupImage()
+        setupData()
     }
     
     private func setupImage() {
-        view.addSubview(avatarImageView)
+        subView1.addSubview(avatarImageView)
         
         avatarImageView.translatesAutoresizingMaskIntoConstraints = false
         avatarImageView.contentMode = .scaleAspectFit
 //
-//        avatarImageView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
-//        avatarImageView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 40).isActive = true
-//        avatarImageView.widthAnchor.constraint(equalTo: scrollView.widthAnchor, multiplier: 0.9).isActive = true
-//        avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor).isActive = true
+        avatarImageView.centerXAnchor.constraint(equalTo: subView1.centerXAnchor).isActive = true
+        avatarImageView.topAnchor.constraint(equalTo: subView1.topAnchor, constant: 40).isActive = true
+        avatarImageView.widthAnchor.constraint(equalTo: subView1.widthAnchor, multiplier: 0.9).isActive = true
+        avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor).isActive = true
         
 //        avatarImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
 //        avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 40).isActive = true
@@ -156,6 +165,7 @@ extension DetailVC {
     private func setupScrollView() {
         let margins = view.layoutMarginsGuide
         view.addSubview(scrollView)
+        
         scrollView.addSubview(scrollStackViewContainer)
         scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
@@ -175,25 +185,3 @@ extension DetailVC {
         scrollStackViewContainer.addArrangedSubview(subview3)
     }
 }
-//extension DetailVC {
-//    private func setupScrollView() {
-//        print("setup scroll view")
-//        scrollView.translatesAutoresizingMaskIntoConstraints = false
-//        contentView.translatesAutoresizingMaskIntoConstraints = false
-//
-//        view.addSubview(scrollView)
-//        scrollView.addSubview(contentView)
-//
-//        scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-//        scrollView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-//        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-//        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-//
-//        contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
-//        contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
-//        contentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
-//        contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
-//
-//        contentView.backgroundColor = .green
-//    }
-//}
