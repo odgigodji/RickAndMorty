918d4663f13a62f6aa925e68e95aeac00f0b426c
nikita evdokimov <evdokimovnikita1@gmail.com>
2022-04-30 03:23:21 +0400

    delete some comments

--- RickAndMorthy/View/DetailVC.swift
+++ RickAndMorthy/View/DetailVC.swift
@@ -30,7 +30,6 @@ final class DetailVC: UIViewController {
         let IV = CustomImageView()
         IV.translatesAutoresizingMaskIntoConstraints = false
         IV.widthAnchor.constraint(equalToConstant: 400).isActive = true
-//        IV.widthAnchor.constraint(equalTo: IV.widthAnchor, multiplier: 0.9).isActive = true
         IV.heightAnchor.constraint(equalTo: IV.widthAnchor,constant: 20).isActive = true
         return IV
     }()
@@ -46,22 +45,7 @@ final class DetailVC: UIViewController {
         return label
     }()
     
-    private func setLabel() {
-        guard let character = character else {
-            descriptionLabel.text = "out of data"
-            return
-        }
-        
-        descriptionLabel.text = """
-name: \(String(character.name!))
-species: \(String(character.species!))
-gender: \(String(character.gender!))
-status: \(String(character.status!))
-location: -
-amount of episodes: -
-"""
-    }
-    
+    //MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGray2
@@ -76,6 +60,8 @@ amount of episodes: -
             avatarImageView.loadImage(from: url)
         }
     }
+    
+    
 }
 
 //MARK: - setupScrollView
@@ -95,6 +81,21 @@ extension DetailVC {
         scrollStackViewContainer.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
         scrollStackViewContainer.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
         configureContainerView()
+    }
+    
+    private func setLabel() {
+        guard let character = character else {
+            descriptionLabel.text = "out of data"
+            return
+        }
+        descriptionLabel.text = """
+name: \(String(character.name!))
+species: \(String(character.species!))
+gender: \(String(character.gender!))
+status: \(String(character.status!))
+location: -
+amount of episodes: -
+"""
     }
     
     private func configureContainerView() {
